b1a1ed91a48bfb88563dbca3beed94d91a9c6089
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-09-04 13:16:18 +0200

    Add comments and singleton pattern

--- src/model/model.ts
+++ src/model/model.ts
@@ -15,52 +15,81 @@ class StructToken {
 }
 
 class Character {
-  name: ObservableValue<string>;
-  scenes: ObservableArray<number>;
-  tags: ObservableArray<string>;
-  actionLength: ObservableValue<number>;
-  dialogueLength: ObservableValue<number>;
-  color: ObservableValue<string>;
+  name: ObservableValue<string>; // character name
+  scenes: ObservableArray<number>; // scenes numbers the character appears in
+  tags: ObservableArray<string>; // character tags (e.g. "protagonist, male")
+  actionLength: ObservableValue<number>; // length of action in characters
+  dialogueLength: ObservableValue<number>; // length of dialogue in characters
+  color: ObservableValue<string>; // color of character 
 }
 
 class Location {
-  name: ObservableValue<string>;
-  scenes: ObservableArray<number>;
+  name: ObservableValue<string>; // location name
+  scenes: ObservableArray<number>; // scenes numbers the location appears in
 }
 
 class Scene {
-  scene: ObservableValue<string>;
-  text: ObservableValue<string>;
-  line: ObservableValue<number>;
-  actionLength: ObservableValue<number>;
-  dialogueLength: ObservableValue<number>;
-  interior: ObservableValue<boolean>;
-  exterior: ObservableValue<boolean>;
-  timeOfDay: ObservableValue<string>;
+  name: ObservableValue<string>; // scene name
+  text: ObservableValue<string>; // scene text? not sure what exactly didn't look into it
+  line: ObservableValue<number>; // line number of scene
+  actionLength: ObservableValue<number>; // length of action in characters
+  dialogueLength: ObservableValue<number>; // length of dialogue in characters
+  interior: ObservableValue<boolean>; // is the scene interior
+  exterior: ObservableValue<boolean>; // is the scene exterior
+  timeOfDay: ObservableValue<string>; // time of day of scene
 }
 
 class ScreenplayProperties {
-  sceneLines: ObservableArray<number>; // is this needed?
-  sceneNames: ObservableArray<string>; // is this needed?
-  titleKeys: ObservableArray<string>; // what is this?
-  firstTokenLine: ObservableValue<number>; // what is this?
-  fontLine: ObservableValue<number>; // what is this?
   lengthAction: ObservableValue<number>; // length of action in characters
   lengthDialogue: ObservableValue<number>; // length of dialogue in characters
   scenes: ObservableArray<Scene>; // array of all scenes
   characters: ObservableArray<Character>; // array of all characters
-  locations: ObservableArray<Location>; // array of all locations
-  structure: StructToken[]; // what is this?
+  locations: ObservableArray<Location>; // array of all locations 
+
+  /**
+   * Binary search for the scene at the given line number
+   * @param line line number to search for
+   * @returns scene at the given line number or null if not found
+   */
+  public getScene(line: number): Scene | null {
+    let scenes = this.scenes.get();
+    let low = 0;
+    let high = scenes.length - 1;
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+      if (scenes[mid].line.get() < line) {
+        low = mid + 1;
+      } else if (scenes[mid].line.get() > line) {
+        high = mid - 1;
+      } else {
+        return scenes[mid];
+      }
+    }
+    return null;
+  }
 }
 
 export class Script {
-  scriptHtml: string;
-  titleHtml: string;
-  titlePage: { [index: string]: token[]; };
-  tokens: token[];
-  tokenLines: { [line: number]: number; };
-  lengthAction: number; // ditch?
-  lengthDialogue: number; // ditch?
-  parseTime: number; // ditch?
-  properties: ScreenplayProperties;
+  private static instance: Script | null = null;
+
+  scriptHtml: string = "";
+  titleHtml: string = "";
+  titlePage: { [index: string]: token[]; } = {};
+  tokens: token[] = [];
+  tokenLines: { [line: number]: number; } = {};
+  parseTime: number = 0; 
+  firstTokenLine: number = 0; 
+  titleKeys: string[] = []; 
+  fontLine: number = 0; 
+  structure: StructToken[] = []; 
+  properties: ScreenplayProperties = new ScreenplayProperties();
+
+  private constructor() {} // Singleton
+
+  public static getInstance(): Script {
+    if (Script.instance === null) {
+      Script.instance = new Script();
+    }
+    return Script.instance;
+  }
 }
